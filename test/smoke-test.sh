70d2d9dd60db262fb0a2996259abe7544ee77633
Jorge Castro <nikey_es@yahoo.es>
2025-11-09 18:12:32 +0100

    Github Action

--- test/smoke-test.sh
+++ test/smoke-test.sh
@@ -33,8 +33,8 @@ section "Test 1: thoughts-init creates directory structure"
 cd "$TEST_DIR"
 setup_git_repo "$TEST_DIR"
 
-# Run thoughts-init in non-interactive mode
-THOUGHTS_NONINTERACTIVE=1 thoughts-init > /dev/null 2>&1
+# Run thoughts-init (always non-interactive)
+thoughts-init > /dev/null 2>&1
 
 assert_dir_exists "thoughts/nikey_es/tickets" "thoughts/{user}/tickets/ created"
 assert_dir_exists "thoughts/nikey_es/notes" "thoughts/{user}/notes/ created"
